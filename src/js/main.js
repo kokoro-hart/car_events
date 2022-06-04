9034210e4223e0a595f3df6f14f80ae150254083
kokoro <cocosyu.11261126@gmail.com>
2022-06-04 09:58:08 +0900

    Remove: 不要なファイルを削除

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,13 @@
 
 /**
- *  エントリポイントファイル
+ *  エントリポイント
  */
 
-
-
 // jsモジュールの読み込み
-import dummyModule from './modules/dummy'
 import './modules/resize'
 import './modules/swiper'
 import './modules/drawer'
 
-
-// tsモジュールの読み込み
-import dummyTsModule from './ts/dummy'
-
 // scssを読み込み
 import '../scss/style.scss'
-
-dummyModule();
-console.log(dummyTsModule(12, 9));
-
 
